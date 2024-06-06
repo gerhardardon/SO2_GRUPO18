3269564fdb817590a72c2ca8de5aa9cc7960d4d7
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2024-06-06 02:03:51 -0600

    Update child2.c

--- Practica1/child2.c
+++ Practica1/child2.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 int main() {
-    // Estos son los parámetros que queremos enviar
+    
     const char *params = "Open";
 
-    // Escribimos los parámetros a la tubería
-    int pipe_fd = 1;  // Asumimos que el descriptor de archivo 1 es la escritura de la tubería
+   
+    int pipe_fd = 1;  
 
     write(pipe_fd, params, strlen(params));
     close(pipe_fd);
