b80914fdf4ecf76f8af2d2448247e10c7fac8891
Cristian Daniel Gómez Escobar <98928867+Cris1928@users.noreply.github.com>
2024-06-15 10:55:50 -0600

    Update read_json.h

--- Practica2/read_json.h
+++ Practica2/read_json.h
@@ -1,5 +1,13 @@
 #ifndef READ_JSON_H
 #define READ_JSON_H
+struct data_struct
+{ //  creamos una estructura para guardar los datos de los usuarios
+    int no_cuenta;
+    char nombre[50];
+    float saldo;
+};
 void read_json_file(char* filename);
+extern int usuarios_size;
+extern struct data_struct usuarios[200];
 
 #endif
