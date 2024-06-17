35cf833b716a391227824391618e485f69feeb16
gerhardardon <gerhardardon@gmail.com>
2024-06-16 21:22:40 -0600

    [ADD] transacciones base

--- Practica2/main.c
+++ Practica2/main.c
@@ -2,15 +2,17 @@
 #include "read_json.h"
 #include "operaciones_indv.h"
 
-int main() {
+int main()
+{
     int opcion;
 
-    do {
+    do
+    {
         // Imprimir el menú
-        printf("Selecciona una opción:\n"); 
+        printf("Selecciona una opción:\n");
         printf("1. Carga masiva de usuarios\n");
         printf("2. Operaciones individuales\n");
-        printf("3. Opcion 3\n");
+        printf("3. Carga masiva de transacciones\n");
         printf("4. Salir\n");
 
         // Solicitar la opción al usuario
@@ -18,19 +20,20 @@ int main() {
         scanf("%d", &opcion);
 
         // Realizar la acción según la opción seleccionada
-        switch (opcion) {
-            case 1:
-                char filename[100];
-                printf("Ingresa la ruta del archivo: ");
-                scanf("%s", filename);
-                
-                
-                read_json_file(filename); //  leemos el archivo json
-                break;
-            case 2:
+        switch (opcion)
+        {
+        case 1:
+            char filename[100];
+            printf("Ingresa la ruta del archivo: ");
+            scanf("%s", filename);
+
+            read_json_file(filename); //  leemos el archivo json
+            break;
+        case 2:
             int subopcion;
-            do{
-                printf("MENU OPERACIONES INDIVIDUALES\n"); 
+            do
+            {
+                printf("MENU OPERACIONES INDIVIDUALES\n");
                 printf("1. Deposito\n");
                 printf("2. Retiro\n");
                 printf("3. Transacciòn\n");
@@ -38,64 +41,70 @@ int main() {
                 printf("\n");
                 printf("Ingresa el número de opción: ");
                 scanf("%d", &subopcion);
-                switch (subopcion) {
-                    case 1:
-                        printf("-------Deposito-------\n");
-                        printf("Ingresa el número de cuenta: ");
-                        int no_cuenta;
-                        scanf("%d", &no_cuenta);
-                        printf("Ingresa el monto: ");
-                        float monto;
-                        scanf("%f", &monto);
-                        depositos(no_cuenta, monto);
-                        break;
-                    case 2:
-                        printf("-------Retiro-------\n");
-                        printf("Ingresa el número de cuenta: ");
-                        int no_cuentaa;
-                        scanf("%d", &no_cuentaa);
-                        printf("Ingresa el monto: ");
-                        float montoo;
-                        scanf("%f", &montoo);
-                        retiros(no_cuentaa, montoo);
-                        break;
-                    case 3:
-                        printf("-------Transacciòn-------\n");
-                        printf("Ingresa el número de cuenta origen: ");
-                        int no_cuenta_origen;
-                        scanf("%d", &no_cuenta_origen);
-                        printf("Ingresa el número de cuenta destino: ");
-                        int no_cuenta_destino;
-                        scanf("%d", &no_cuenta_destino);
-                        printf("Ingresa el monto: ");
-                        float montooo;
-                        scanf("%f", &montooo);
-                        transacciones(no_cuenta_origen, no_cuenta_destino, montooo);
-                        break;
-                    case 4:
-                        printf("-------Consultar cuenta-------\n");
-                        printf("Ingresa el número de cuenta: ");
-                        int no_cuentaaaa;
-                        scanf("%d", &no_cuentaaaa);
-                        consultar_cuenta(no_cuentaaaa);
-                        break;
-                    default:
-                        printf("Opción inválida. Por favor, selecciona una opción válida.\n");
-                        break;
-                } printf("\n");
-    } while (subopcion != 4);
+                switch (subopcion)
+                {
+                case 1:
+                    printf("-------Deposito-------\n");
+                    printf("Ingresa el número de cuenta: ");
+                    int no_cuenta;
+                    scanf("%d", &no_cuenta);
+                    printf("Ingresa el monto: ");
+                    float monto;
+                    scanf("%f", &monto);
+                    depositos(no_cuenta, monto);
+                    break;
+                case 2:
+                    printf("-------Retiro-------\n");
+                    printf("Ingresa el número de cuenta: ");
+                    int no_cuentaa;
+                    scanf("%d", &no_cuentaa);
+                    printf("Ingresa el monto: ");
+                    float montoo;
+                    scanf("%f", &montoo);
+                    retiros(no_cuentaa, montoo);
+                    break;
+                case 3:
+                    printf("-------Transacciòn-------\n");
+                    printf("Ingresa el número de cuenta origen: ");
+                    int no_cuenta_origen;
+                    scanf("%d", &no_cuenta_origen);
+                    printf("Ingresa el número de cuenta destino: ");
+                    int no_cuenta_destino;
+                    scanf("%d", &no_cuenta_destino);
+                    printf("Ingresa el monto: ");
+                    float montooo;
+                    scanf("%f", &montooo);
+                    transacciones(no_cuenta_origen, no_cuenta_destino, montooo);
+                    break;
+                case 4:
+                    printf("-------Consultar cuenta-------\n");
+                    printf("Ingresa el número de cuenta: ");
+                    int no_cuentaaaa;
+                    scanf("%d", &no_cuentaaaa);
+                    consultar_cuenta(no_cuentaaaa);
+                    break;
+                default:
+                    printf("Opción inválida. Por favor, selecciona una opción válida.\n");
+                    break;
+                }
+                printf("\n");
+            } while (subopcion != 4);
 
-                break;
+            break;
 
-            case 3:
-                printf("Seleccionaste la Opcion 3\n");
-                break;
-            case 4:
-                printf("Saliendo del programa...\n");
-                break;
-            default:
-                printf("Opción inválida. Por favor, selecciona una opción válida.\n");
-                break;
+        case 3:
+            char filename2[100];
+            printf("\nIngresa la ruta del archivo: ");
+            scanf("%s", filename2);
+            printf("Cargando transacciones...\n");
+            break;
+
+        case 4:
+            printf("Saliendo del programa...\n");
+            break;
+        default:
+            printf("Opción inválida. Por favor, selecciona una opción válida.\n");
+            break;
         }
 
         printf("\n");
